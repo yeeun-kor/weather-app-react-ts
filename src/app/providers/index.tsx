82b8fee55e831205934844e64397fe65269bc4c2
yeeun <jovm268@naver.com>
2026-01-15 20:07:55 +0900

    chore : 쿼리 provider 설정

---
+++ src/app/providers/index.tsx
@@ -0,0 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { type ReactNode } from "react";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, //초기화 타임 5분
+      retry: 1,
+    },
+  },
+});
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+export function Providers({ children }: ProvidersProps) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
